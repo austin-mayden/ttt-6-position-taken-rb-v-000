d6e3a3fea4f42fd63240687fcfec3ed723b0f8a5
Austin Mayden <austin.mayden@gmail.com>
2019-08-09 20:56:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 def position_taken?(board, index)
-  board[index] == " " || board[index] == ""
-  false
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  else
+    nil
+  end
 end
